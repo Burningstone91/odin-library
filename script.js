6d7e8937f4e954d3a684a0a9052e2b81685f4a2a
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-03-30 13:43:10 +0100

    Remove unneded info function and rename array to more meaningful 'library'

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const myArray = [];
+const library = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-      return this.title + 
-        " by " + 
-        this.author + 
-        ", " + 
-        this.pages + 
-        " pages, " + 
-        (read == true ? "read" : "not read yet")
-  };
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read)
-  myArray.push(newBook);
+  const newBook = new Book(title, author, pages, read);
+  library.push(newBook);
+}
+
+function showBooks () {
+
 }
 
 console.log(myArray);
